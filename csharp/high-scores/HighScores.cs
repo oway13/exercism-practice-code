abd35ec394bfa8f74c37eb8a00489ac6884e2242
Oway13 <wyattcody133@gmail.com>
2019-02-04 12:33:21 -0600

    Completed HighScores

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -4,33 +4,78 @@ using System.Linq;
 
 public class HighScores
 {
+
+    private List<int> highScores;
+    private List<int> topScores;
+
+
+    //Calls PersonalTop() when the score list is constructed so that 
+    //the O(n) function doesn't need to be called many times in the various
+    //other functions that use its results.
     public HighScores(List<int> list)
     {
-        throw new NotImplementedException();
+        highScores = list;
+        topScores = this.PersonalTop();
     }
 
     public List<int> Scores()
     {
-        throw new NotImplementedException();
+        return highScores;
     }
 
     public int Latest()
     {
-        throw new NotImplementedException();
+        return highScores[highScores.Count-1];
     }
 
     public int PersonalBest()
     {
-        throw new NotImplementedException();
+        return topScores[0];
     }
 
+
+    //Loops through entire list, comparing each element with the current top three 
+    //scores, replacing and bumping down current top scores. Adds non-negative
+    //top values to the return list. 
     public List<int> PersonalTop()
     {
-        throw new NotImplementedException();
+        int top1 = -1;
+        int top2 = -1;
+        int top3 = -1;
+        List<int> topScores = new List<int>();
+        for (int i = 0; i < highScores.Count; i++){
+            if (highScores[i] > top1){
+                top3 = top2;
+                top2 = top1;
+                top1 = highScores[i];
+            } else if (highScores[i] > top2) {
+                top3 = top2;
+                top2 = highScores[i];
+            } else if (highScores[i] > top3) {
+                top3 = highScores[i];
+            }
+        }
+        topScores.Add(top1);
+        if (top2 != -1) {
+            topScores.Add(top2);
+        } 
+        if (top3 != -1) {
+            topScores.Add(top3);
+        }
+        return topScores;
     }
 
     public string Report()
     {
-        throw new NotImplementedException();
+        int latest = this.Latest();
+        int best = this.PersonalBest();
+        int difference = best - latest;
+        if(difference == 0){
+            return String.Format("Your latest score was {0}. " +
+             "That's your personal best!", latest);
+        } else {
+            return String.Format("Your latest score was {0}. " +
+            "That's {1} short of your personal best!", latest, difference);
+        }
     }
 }
--- csharp/high-scores/HighScoresTest.cs
+++ csharp/high-scores/HighScoresTest.cs
@@ -12,77 +12,77 @@ public class HighScoresTest
         Assert.Equal(new List<int> { 30, 50, 20, 70 }, sut.Scores());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Latest_score()
     {
         var sut = new HighScores(new List<int> { 100, 0, 90, 30 });
         Assert.Equal(30, sut.Latest());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_best()
     {
         var sut = new HighScores(new List<int> { 40, 100, 70 });
         Assert.Equal(100, sut.PersonalBest());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_top()
     {
         var sut = new HighScores(new List<int> { 50, 30, 10 });
         Assert.Equal(new List<int> { 50, 30, 10 }, sut.PersonalTop());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_top_highest_to_lowest()
     {
         var sut = new HighScores(new List<int> { 20, 10, 30 });
         Assert.Equal(new List<int> { 30, 20, 10 }, sut.PersonalTop());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_top_when_there_is_a_tie()
     {
         var sut = new HighScores(new List<int> { 40, 20, 40, 30 });
         Assert.Equal(new List<int> { 40, 40, 30 }, sut.PersonalTop());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_top_when_there_are_less_than_3()
     {
         var sut = new HighScores(new List<int> { 30, 70 });
         Assert.Equal(new List<int> { 70, 30 }, sut.PersonalTop());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_top_when_there_is_only_one()
     {
         var sut = new HighScores(new List<int> { 40 });
         Assert.Equal(new List<int> { 40 }, sut.PersonalTop());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Personal_top_from_a_long_list()
     {
         var sut = new HighScores(new List<int> { 10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70 });
         Assert.Equal(new List<int> { 100, 90, 70 }, sut.PersonalTop());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Message_for_new_personal_best()
     {
         var sut = new HighScores(new List<int> { 20, 40, 0, 30, 70 });
         Assert.Equal("Your latest score was 70. That's your personal best!", sut.Report());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Message_when_latest_score_is_not_the_highest_score()
     {
         var sut = new HighScores(new List<int> { 20, 100, 0, 30, 70 });
         Assert.Equal("Your latest score was 70. That's 30 short of your personal best!", sut.Report());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Message_for_repeated_personal_best()
     {
         var sut = new HighScores(new List<int> { 20, 70, 50, 70, 30 });
