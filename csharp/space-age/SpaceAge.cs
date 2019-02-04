e72e739c6f45d0f35386416f9ee22db639998c37
Oway13 <wyattcody133@gmail.com>
2019-02-04 11:57:38 -0600

    Completed SpaceAge

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,47 +2,52 @@ using System;
 
 public class SpaceAge
 {
+    private int secs;
+    private double earthyears;
+
     public SpaceAge(int seconds)
     {
+        secs = seconds;
+        earthyears = secs/31557600.0;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears, 2);
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/0.2408467, 2); 
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/0.61519726, 2); 
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/1.8808158, 2); 
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/11.862615, 2); 
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/29.447498, 2); 
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/84.016846, 2); 
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(earthyears/164.79132, 2); 
     }
 }
