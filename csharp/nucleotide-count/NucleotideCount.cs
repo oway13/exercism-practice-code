aff2fa3c20549d388bccac97c07e157db00bf8c8
Oway13 <wyattcody133@gmail.com>
2019-02-11 11:48:29 -0600

    Completed Nucleotide Count

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,6 +5,19 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Dictionary<char, int> counts = new Dictionary<char, int>{
+            ['A'] = 0,
+            ['C'] = 0,
+            ['G'] = 0,
+            ['T'] = 0
+        };
+
+        for (int i = 0; i < sequence.Length; i++){
+            if (!counts.ContainsKey(sequence[i])){
+                throw new ArgumentException("Sequence contains invalid nucleotide. Valid nucleotides are 'A', 'C', 'G', 'T'.");
+            }
+            counts[sequence[i]]++;
+        }
+        return counts;
     }
 }
