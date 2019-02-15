7f202eec22f8f445f2c5e9460e1d53c934717493
Oway13 <wyattcody133@gmail.com>
2019-02-15 16:35:02 -0600

    Downloaded RobotName

---
+++ csharp/robot-name/RobotName.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Robot
+{
+    private static HashSet<string> names = new HashSet<string>();
+    private Random rnd = new Random();
+    public string Name
+    {
+        get
+        {
+           throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
