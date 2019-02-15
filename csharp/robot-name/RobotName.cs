f91b7da43641704a2b1dd5844f8f6d17b62f2284
Oway13 <wyattcody133@gmail.com>
2019-02-15 17:00:31 -0600

    Completed RobotName

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,19 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
     private static HashSet<string> names = new HashSet<string>();
     private Random rnd = new Random();
+    private string _name;
     public string Name
     {
         get
         {
-           throw new NotImplementedException("You need to implement this function.");
+            if (_name != null) return _name;
+            string nme = getLetter()+getLetter()+getNumber()+getNumber()+getNumber();
+            while(names.Contains(nme)){
+                nme = getLetter()+getLetter()+getNumber()+getNumber()+getNumber();
+            }
+            _name = nme;
+            names.Add(_name);
+            return nme;
         }
     }
 
+    private string getLetter(){
+        int num = rnd.Next(0,26);
+        return ((char)('A' + num)).ToString();
+    }
+    private string getNumber(){
+        return rnd.Next(0,10).ToString();
+    }
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        names.Remove(_name);
+        _name = null;
     }
 }
